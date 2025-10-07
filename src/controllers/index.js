0084f4efc7ef7bb3affd292cdf4dfa0a30af1d66
MeliAlvarez14 <melialvarez180@gmail.com>
2025-10-07 01:32:56 -0300

    importar controladores de la entidad Especialidad

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,11 @@
 const telefonoController = require('./telefonoController')
 const emailController = require('./emailController')
 const provinciaController = require('./provinciaController')
+const especialidadController = require('./especialidadController')
 
 module.exports = {
   telefonoController,
   emailController,
-  provinciaController
+  provinciaController,
+  especialidadController
 }
