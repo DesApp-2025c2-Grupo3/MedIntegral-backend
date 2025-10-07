1cb6244474f1140fd9440c688b1471488e5ce723
MeliAlvarez14 <melialvarez180@gmail.com>
2025-10-07 02:14:16 -0300

    importar controladores de la entidad Direccion

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,11 @@
 const telefonoController = require('./telefonoController')
 const emailController = require('./emailController')
 const provinciaController = require('./provinciaController')
+const direccionController = require('./direccionController')
 
 module.exports = {
   telefonoController,
   emailController,
-  provinciaController
+  provinciaController,
+  direccionController
 }
