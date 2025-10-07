44f9370ef04697480bbc351724fac8a64c42fe25
MeliAlvarez14 <melialvarez180@gmail.com>
2025-10-07 01:03:59 -0300

    importar controladores de la entidad Provincia

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,9 @@
 const telefonoController = require('./telefonoController')
 const emailController = require('./emailController')
+const provinciaController = require('./provinciaController')
 
 module.exports = {
   telefonoController,
-  emailController
+  emailController,
+  provinciaController
 }
