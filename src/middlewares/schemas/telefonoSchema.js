e5dc9e6be2573a2c0830f6c5ca4706f6e799182a
gabrielledezma21 <gabrielagustin.ledezma@estudiantes.unahur.edu.ar>
2025-10-08 02:05:33 -0300

    cambios en schema telefono

--- src/middlewares/schemas/telefonoSchema.js
+++ src/middlewares/schemas/telefonoSchema.js
@@ -7,11 +7,11 @@ const schemaCreate = Joi.object({
     .max(12)
     .required()
     .messages({
-      'any.required': 'Es obligatorio ingresar al menos un número de teléfono',
       'number.base': 'El teléfono debe ser un número',
       'number.integer': 'El teléfono debe contener sólo números',
       'number.min': 'El teléfono debe contener al menos {#limit} dígitos',
-      'number.max': 'El teléfono debe contener como máximo {#limit} dígitos'
+      'number.max': 'El teléfono debe contener como máximo {#limit} dígitos',
+      'any.required': 'Es obligatorio ingresar al menos un número de teléfono'
     })
 })
 
