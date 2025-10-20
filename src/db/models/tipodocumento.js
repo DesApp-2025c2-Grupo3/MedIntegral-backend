67768eea2fa31cfa3f7d11308127defd84d993a0
alymarquez <alyym.55@gmail.com>
2025-10-20 16:12:00 -0300

    Modelo tipo documento

---
+++ src/db/models/tipodocumento.js
@@ -0,0 +1,17 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class TipoDocumento extends Model {
+    static associate(models) {
+    }
+  }
+  TipoDocumento.init({
+    tipo: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'TipoDocumento',
+  });
+  return TipoDocumento;
+};
