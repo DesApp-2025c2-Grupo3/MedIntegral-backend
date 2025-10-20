4a92b931f3a9fa8dca9434142dc025544a9923a7
alymarquez <alyym.55@gmail.com>
2025-10-20 16:04:24 -0300

    Modelo situaciones terapeuticas

---
+++ src/db/seeders/20251020190017-inicializar-situaciones-terapeuticas.js
@@ -0,0 +1,77 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "SituacionesTerapeuticas",
+      [
+        {
+          nombre: "Diabetes tipo 1",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nombre: "Diabetes tipo 2",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nombre: "Hipertensión Arterial",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nombre: "Enfermedad Celíaca",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nombre: "Embarazo",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nombre: "Tratamiento Oncológico",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nombre: "Insuficiencia Renal Crónica",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nombre: "Discapacidad Certificada (CUD)",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nombre: "Trasplante de Órgano",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nombre: "VIH/SIDA",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nombre: "Patología de Columna",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nombre: "Asma Severa",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('SituacionesTerapeuticas', null, {});
+  },
+};
