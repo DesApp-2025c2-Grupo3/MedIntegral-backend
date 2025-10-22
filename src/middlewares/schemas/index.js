be73e7ca4cd3dd7398da85037fc8d5c22e452e00
gabrielledezma21 <gabrielagustin.ledezma@estudiantes.unahur.edu.ar>
2025-10-22 03:03:38 -0300

    borrado de schema de telefono ya que no se utilizaba

--- src/middlewares/schemas/index.js
+++ src/middlewares/schemas/index.js
@@ -1,9 +1,7 @@
-const telefonoSchema = require('./telefonoSchema')
 const prestadorSchema = require('./prestadorSchema')
 const agendaTurnosSchema  = require('./agendaTurnosSchema')
 
 module.exports = {
-  telefonoSchema,
   prestadorSchema,
   agendaTurnosSchema
 }
