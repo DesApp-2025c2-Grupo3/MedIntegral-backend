4a8a772c1fda1606ef444936a1678dfcd313cdfe
gabrielledezma21 <gabrielagustin.ledezma@estudiantes.unahur.edu.ar>
2025-10-31 22:04:54 -0300

    mas formateo

--- src/controllers/prestadorController.js
+++ src/controllers/prestadorController.js
@@ -100,12 +100,12 @@ const obtenerPrestadores = async (_, res) => {
       exclude: ["createdAt", "updatedAt"],
     },
     include: [
-      { model: Email, attributes: ["direccion"] },
-      { model: Telefono, attributes: ["numero"] },
+      { model: Email, attributes: ["id","direccion"] },
+      { model: Telefono, attributes: ["id","numero"] },
       {
         model: Especialidad,
         as: "Especialidad",
-        attributes: ["nombre"],
+        attributes: ["id","nombre"],
         through: { attributes: [] },
       },
       {
