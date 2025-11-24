68c04b3938a9a5db45280a65368cfbea023a5184
alymarquez <alyym.55@gmail.com>
2025-11-23 22:10:13 -0300

    Fix para funcionamiento de detalle/edicion afiliado

--- src/controllers/afiliadoController.js
+++ src/controllers/afiliadoController.js
@@ -134,12 +134,15 @@ const crearAfiliado = async (req, res) => {
     let nIntegrante = 2;
 
     for (const miembro of grupoFamiliar) {
+      const capitalizedNombre = await capitalizarCadena(miembro.nombre);
+      const capitalizedApellido = await capitalizarCadena(miembro.apellido);
+
       const nuevoIntegrante = await Afiliado.create({
         tipoDocumentoId: miembro.tipoDocumentoId,
         numeroDocumento: miembro.numeroDocumento,
         fechaNacimiento: miembro.fechaNacimiento,
-        nombre: miembro.nombre,
-        apellido: miembro.apellido,
+        nombre: capitalizedNombre,
+        apellido: capitalizedApellido,
         vigenciaInicio: miembro.vigenciaInicio,
         vigenciaFin: miembro.vigenciaFin,
         nIntegrante: nIntegrante,
@@ -638,6 +641,7 @@ const crearDirecciones = async (direcciones, afiliadoId) => {
       ...direccionData,
       calle: calleCapitalizada,
       localidad: localidadCapitalizada,
+      provinciaId: direccionData.provinciaId
     };
 
     const [direccion] = await Direccion.findOrCreate({
@@ -645,8 +649,9 @@ const crearDirecciones = async (direcciones, afiliadoId) => {
         calle: calleCapitalizada,
         altura: direccionData.altura,
         pisoDepto: direccionData.pisoDepto,
-        localidad: direccionData.localidad,
+        localidad: localidadCapitalizada,
         codigoPostal: direccionData.codigoPostal,
+        provinciaId: direccionData.provinciaId
       },
       defaults: datosParaCrear,
     });
