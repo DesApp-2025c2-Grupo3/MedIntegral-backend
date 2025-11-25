25733ed03dc3c69391987a57b92b6b0f0f70c747
MeliAlvarez14 <melialvarez180@gmail.com>
2025-11-25 01:06:45 -0300

    Refactor función que genera PDF para que sea genérica

--- src/utils/pdfGenerator.js
+++ src/utils/pdfGenerator.js
@@ -1,25 +1,38 @@
 const PDFDocument = require('pdfkit-table');
 
-const generarReporteAfiliadoPDF = (dataCallBack, endCallBack) => {
+const generarReporteAfiliadoPDF = (data, dataCallBack, endCallBack) => {
     const pdf = new PDFDocument({ margin: 30, size: 'A4' })
 
     pdf.on('data', dataCallBack);
     pdf.on('end', endCallBack);
 
-    pdf.fontSize(20).text('Reporte Situaciones Terapéuticas');
+    const x = pdf.x;
+    const y = pdf.y;
+
+    pdf.image('public/logo.png', x + 120, y, {width: 60})
+    .fontSize(30).text('MedIntegral', x, y + 20, { align: 'center' });
+    pdf.moveDown();
+
+    pdf.fontSize(15).text(data.tittle);
+    pdf.moveDown();
 
     const tableArray = {
-        headers: ["Afiliado/s", "DNI", "Situaciones Terapéuticas", "Vigencia Inicio", "Vigencia Fin"],
-        rows: [
-        ["Alvarez Melina", "46569284", "Diabetes", "20/10/2025", "20/10/2026"],
-        ["Alvarez Luciano", "48878921", "Embarazo", "20/10/2025", "20/10/2026"],
-        ["Alvarez Nicolas", "51493711", "Embarazo", "20/10/2025", "-"],
-        ],
+        headers: data.headers, 
+        datas: data.datas
     };
+
     pdf.table( tableArray, {
-    prepareHeader: () => pdf.fontSize(12),
-    prepareRow: (row, i) => pdf.fontSize(10),
-  });
+            rowsHeight: 25,
+            prepareHeader: () => pdf.fontSize(12),
+            prepareRow: (row, indexColumn, indexRow, rectRow) => {
+                pdf.fontSize(10);
+                pdf.text(row[indexColumn], rectRow.x, rectRow.y, {
+                    width: rectRow.width,
+                    align: "center" 
+                });
+            },
+        }
+    );
 
     pdf.end()
 }
